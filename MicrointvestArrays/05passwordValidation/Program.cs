db4d72795d2d6693a3974a13de54816d1ea7fceb
BorislavVladimirov <49073192+BorislavVladimirov@users.noreply.github.com>
2019-07-13 23:29:47 +0300

    Added changes in 05passwordValidation/Program.cs

--- MicrointvestArrays/05passwordValidation/Program.cs
+++ MicrointvestArrays/05passwordValidation/Program.cs
@@ -7,62 +7,7 @@ namespace _05passwordValidation
     {
         public static void Main(string[] args)
         {
-            char[] password = Console.ReadLine().ToCharArray();
-
-            List<string> unfulfilledCriteries = new List<string> {
-                  "поне една цифра"
-                , "поне една малка буква"
-                , "поне една голяма буква"
-                , "минимална дължина от 6 символа"
-                , "поне един символ различен от цифра и буква" };
-
-            IsPasswordValid(password, unfulfilledCriteries);
-
-            if (unfulfilledCriteries.Count == 0)
-            {
-                Console.WriteLine(IsPasswordValid(password, unfulfilledCriteries));
-            }
-            else
-            {
-                Console.OutputEncoding = System.Text.Encoding.Unicode;
-                Console.WriteLine($"{IsPasswordValid(password, unfulfilledCriteries)}, Паролата не изпълнява следните условия: " +
-                    $"{string.Join(", ",unfulfilledCriteries)}");
-            }
-        }
-
-        private static bool IsPasswordValid(char[] password, List<string> unfulfilledCriteries)
-        {
-            if (password.Length >= 6)
-            {
-                unfulfilledCriteries.Remove("минимална дължина от 6 символа");
-            }
-
-            for (int i = 0; i < password.Length; i++)
-            {
-                if (char.IsDigit(password[i]))
-                {
-                    unfulfilledCriteries.Remove("поне една цифра");
-                }
-                else if (char.IsUpper(password[i]))
-                {
-                    unfulfilledCriteries.Remove("поне една голяма буква");
-                }
-                else if (char.IsLower(password[i]))
-                {
-                    unfulfilledCriteries.Remove("поне една малка буква");
-                }
-                else if (!char.IsLetterOrDigit(password[i]))
-                {
-                    unfulfilledCriteries.Remove("поне един символ различен от цифра и буква");
-                }
-            }
-
-            if (unfulfilledCriteries.Count == 0)
-            {
-                return true;
-            }
-
-            return false;
+           
         }
     }
 }
