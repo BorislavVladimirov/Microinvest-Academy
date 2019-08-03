700bab789571e60ae03473169d6a13563375f171
BorislavVladimirov <49073192+BorislavVladimirov@users.noreply.github.com>
2019-08-03 20:15:38 +0300

    Added new project in MicroinvestSortingAlgorithms.sln.

---
+++ MicroinvestSortingAlgorithms/Task03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string stringToCheck = Console.ReadLine();
+
+            int startingIndex = 0;
+            Console.WriteLine(CheckIsPalindrome(stringToCheck, startingIndex));
+        }
+
+        private static bool CheckIsPalindrome(string stringToCheck, int currentFrontIndex)
+        {
+            int currentBackIndex = stringToCheck.Length - 1 - currentFrontIndex;
+
+            if (stringToCheck[currentFrontIndex] != stringToCheck[currentBackIndex])
+            {
+                return false;
+            }
+
+            if (currentFrontIndex == stringToCheck.Length / 2)
+            {
+                return true;
+            }
+
+            return CheckIsPalindrome(stringToCheck, currentFrontIndex + 1);
+        }
+    }
+}
