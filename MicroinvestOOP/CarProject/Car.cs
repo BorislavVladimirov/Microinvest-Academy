354dec2b9d623b639ffc5ed00c4f4f36c8cd9e56
BorislavVladimirov <49073192+BorislavVladimirov@users.noreply.github.com>
2019-09-09 22:45:58 +0300

    Added CarProject in MicroinvestOOP.

---
+++ MicroinvestOOP/CarProject/Car.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarProject
+{
+    public class Car
+    {
+        private string model;
+        private int maxSpeed;
+        private int currentSpeed;
+        private string color;
+        private int currentGear;
+        private Person owner;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string model, int maxSpeed)
+            : this()
+        {
+            Model = model;
+            MaxSpeed = maxSpeed;
+        }
+
+        public Car(string model, int maxSpeed, int currentSpeed, string color, int currentGear, Person owner)
+            : this(model, maxSpeed)
+        {
+            Model = model;
+            MaxSpeed = maxSpeed;
+            CurrentSpeed = currentSpeed;
+            Color = color;
+            CurrentGear = currentGear;
+            Owner = owner;
+        }
+
+        #region Property
+        public string Model
+        {
+            get => this.model;
+
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public int MaxSpeed
+        {
+            get => this.maxSpeed;
+
+            set
+            {
+                this.maxSpeed = value;
+            }
+        }
+
+        public int CurrentSpeed
+        {
+            get => this.currentSpeed;
+
+            set
+            {
+                if (value > this.MaxSpeed)
+                {
+                    Console.WriteLine("Current speed cant't be greater than the vehicle max speed!");
+                    return;
+                }
+
+                this.currentSpeed = value;
+            }
+        }
+
+        public string Color
+        {
+            get => this.color;
+
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        public int CurrentGear
+        {
+            get => this.currentGear;
+
+            set
+            {
+                this.currentGear = value;
+            }
+        }
+
+        public Person Owner
+        {
+            get => this.owner;
+
+            set
+            {
+                this.owner = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MicroinvestOOP/CarProject/CarDemo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarProject
+{
+    public class CarDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Person BMWOwner = new Person("Strahil", 24);
+            Person AudiOwner = new Person("Kolio", 26);
+
+            Car BMW = new Car("M5", 300, 220, "Blue", 6, BMWOwner);
+            Car Audi = new Car("A8", 300, 200, "Black", 8, AudiOwner);
+
+            Console.WriteLine("Въведете пореден номена на колата, на която искате да промените параметрите");
+
+            ChangeCarsProperties(BMW, Audi);
+
+            Console.WriteLine(PrintBMWInfo(BMW));     
+        }
+
+        private static string PrintBMWInfo(Car BMW)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Car model: {BMW.Model}");
+            sb.AppendLine($"Car owner: {BMW.Owner.Name}");
+            sb.AppendLine($"Owner's age {BMW.Owner.Age.ToString()}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void ChangeCarsProperties(Car BMW, Car Audi)
+        {
+            int carNumber;
+
+            int.TryParse(Console.ReadLine(), out carNumber);
+
+            while (carNumber == 1 || carNumber == 2)
+            {
+                int newGear;
+                int newCurrentSpeed;
+
+                switch (carNumber)
+                {
+                    case 1:
+                        Console.WriteLine("Въведете нова предавка");
+
+                        int.TryParse(Console.ReadLine(), out newGear);
+
+                        ChangeGear(newGear, BMW);
+
+                        Console.WriteLine("Въведете нова текуща скорост");
+
+                        int.TryParse(Console.ReadLine(), out newCurrentSpeed);
+
+                        ChangeSpeed(newCurrentSpeed, BMW);
+                        break;
+                    case 2:
+                        Console.WriteLine("Въведете нова предавка");
+
+                        int.TryParse(Console.ReadLine(), out newGear);
+
+                        ChangeGear(newGear, Audi);
+
+                        Console.WriteLine("Въведете нова текуща скорост");
+
+                        int.TryParse(Console.ReadLine(), out newCurrentSpeed);
+
+                        ChangeSpeed(newCurrentSpeed, Audi);
+                        break;
+                }
+
+                Console.WriteLine("Въведете следваща кола");
+
+                int.TryParse(Console.ReadLine(), out carNumber);
+            }
+        }
+
+        private static void ChangeSpeed(int newCurrentSpeed, Car car)
+        {
+            car.CurrentSpeed = newCurrentSpeed;
+        }
+
+        private static void ChangeGear(int newGear, Car car)
+        {
+            car.CurrentGear = newGear;
+        }
+    }
+}
